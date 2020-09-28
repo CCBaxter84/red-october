0c84b8be062e4eb9b7e6f4d8465fae7833efb522
CCBaxter84 <troyceclark@gmail.com>
2020-09-28 13:37:03 -0600

    Updates

---
+++ precache-manifest.bb305f12113a7034ab57180617001fc0.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a174821cec0dad0f68ecf1dc822ac05f",
+    "url": "/red-october/index.html"
+  },
+  {
+    "revision": "20b4c89acc7412460caf",
+    "url": "/red-october/static/css/main.ff96b9fc.chunk.css"
+  },
+  {
+    "revision": "9b948cb74416ef41dd7e",
+    "url": "/red-october/static/js/2.2e409076.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/red-october/static/js/2.2e409076.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20b4c89acc7412460caf",
+    "url": "/red-october/static/js/main.252d4b27.chunk.js"
+  },
+  {
+    "revision": "0282966d11c2be62f486",
+    "url": "/red-october/static/js/runtime-main.17740eda.js"
+  },
+  {
+    "revision": "68ea9528f95ee24fde8e7a0981cc0d99",
+    "url": "/red-october/static/media/atlanticMap.68ea9528.jpg"
+  },
+  {
+    "revision": "a919f60f9504a6917225ffc0af634ae4",
+    "url": "/red-october/static/media/crazy-ivan3.a919f60f.jpg"
+  },
+  {
+    "revision": "a495595bf5b5cc86dbcb1fa1c1f56028",
+    "url": "/red-october/static/media/davenport.a495595b.jpg"
+  },
+  {
+    "revision": "fcc259f54d6bace25c339703a9fb46c3",
+    "url": "/red-october/static/media/game-over3.fcc259f5.jpg"
+  },
+  {
+    "revision": "b15642619fdcc31fdca4c7fdb6aa526a",
+    "url": "/red-october/static/media/game-over4.b1564261.jpg"
+  },
+  {
+    "revision": "35f4dc12fa342f3619b80a8dbbbd769e",
+    "url": "/red-october/static/media/magma3.35f4dc12.jpg"
+  },
+  {
+    "revision": "465e4b8cc9923289a9ed2033f8b81ce4",
+    "url": "/red-october/static/media/mancuso.465e4b8c.jpg"
+  },
+  {
+    "revision": "16d34a9915a291843edc8141ef9ba3dd",
+    "url": "/red-october/static/media/out-of-control.16d34a99.jpg"
+  },
+  {
+    "revision": "ed923d7f5d6f89b7343f9d76366ad537",
+    "url": "/red-october/static/media/plan2.ed923d7f.jpg"
+  },
+  {
+    "revision": "399965b8ed119ad1010db1e1afaa872a",
+    "url": "/red-october/static/media/red-october.399965b8.jpg"
+  },
+  {
+    "revision": "2b81e21ca01036fb70587ffbf9a83254",
+    "url": "/red-october/static/media/skip.2b81e21c.jpg"
+  },
+  {
+    "revision": "1e674f525f844052ea8086be9120ad51",
+    "url": "/red-october/static/media/win2.1e674f52.jpg"
+  }
+]);
